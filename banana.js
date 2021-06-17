fce85c5d675ec8a7ce91ebaade669b4deabcafba
AASHISH KAUSHIK <aashishkaushik_bca18_21@its.edu.in>
2021-06-17 08:47:51 +0530

    update bugs

--- banana.js
+++ banana.js
@@ -1,22 +1,22 @@
-var btntranslater= document.querySelector("#btn-translater");
-var textinput = document.querySelector("#text-input");
-var outputdiv = document.querySelector("#output");
-var serverURL ="https://api.funtranslations.com/translate/groot.json"
-function gettranslationURL(input){
-    return serverURL+"?"+"text"+input
+var btntranslater = document.querySelector("#btn");
+var input = document.querySelector("#input");
+var output = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/groot.json";
+function gettranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
 }
-function errorhandler(error){
-    console.log("error occured",error)
-    alert("something wrong with server! try again after some time")
+function errorhandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
 }
-function clickhnadler(){
-    var inputText = txtInput.value;
-    fetch(translationURL(inputText))
-    .then(response => response.json())
-        .then(json => {
-            var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText;
-           })
-        .catch(errorHandler)
-        btnTranslate.addEventListener("click", clickHandler)
-};
+function clickHandler() {
+  var inputText = input.value;
+  fetch(gettranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      output.innerText = translatedText;
+    })
+    .catch(errorhandler);
+}
+btn.addEventListener("click", clickHandler);
